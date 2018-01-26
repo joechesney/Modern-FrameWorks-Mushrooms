e3f8295c5d7cda764a7f4c3466f8d2f5ce6dd5a1
joechesney <33613119+joechesney@users.noreply.github.com>
2018-01-25 23:47:35 -0600

    cleaned up files. removed console logs

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 "use strict";
-
 const shroomApp = angular.module("ShroomSorter", []);
 
-
 shroomApp.controller("ShroomCtrl", function ($scope, ShroomFactory) {
-  let tester = "wadupu";
-  // console.log('hellloo',tester);
   ShroomFactory.getShrooms()
   .then( shroomsData => {
-    console.log('shrooms2',shroomsData.data);
-    // $scope.mushroomList = shroomsData.data;
     $scope.mushroomList = [];
     for(let mushroom in shroomsData.data){
       $scope.mushroomList.push(shroomsData.data[mushroom]);
     };
-    
-    console.log('shrooms3',$scope.mushroomList);
   })
-
-  const togler = function(){
-    
-  };
 });
 
 
@@ -29,7 +17,6 @@ shroomApp.factory("ShroomFactory", function($q, $http) {
     return $q( (resolve, reject)=> {
       $http.get("https://testetization.firebaseio.com/mushroom.json")
       .then( (shrooms) => {
-        console.log('shrooms1',shrooms);
         resolve(shrooms);
       }) 
       .catch( err => {
